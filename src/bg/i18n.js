08464a6f626e04f418cdaaa1c9b5286da4c61f21
Christopher EnyTC <chris@enytc.com>
2017-04-28 02:08:41 -0300

    Fix indentation

--- src/bg/i18n.js
+++ src/bg/i18n.js
@@ -9,21 +9,21 @@
 
 document.title = chrome.i18n.getMessage('options_title');
 if (document.getElementById('i18n-copyright-year')) {
-    document.getElementById('i18n-copyright-year').innerHTML = 'Copyright &copy; ' + new Date().getFullYear();
+	document.getElementById('i18n-copyright-year').innerHTML = 'Copyright &copy; ' + new Date().getFullYear();
 }
 
 //setMessage
 
 function setMessage(id, key, data) {
-    if (document.getElementById('i18n-' + id)) {
-        document.getElementById('i18n-' + id).innerHTML = chrome.i18n.getMessage(key, data);
-    }
+	if (document.getElementById('i18n-' + id)) {
+		document.getElementById('i18n-' + id).innerHTML = chrome.i18n.getMessage(key, data);
+	}
 }
 
 function setPlaceholder(name, key, data) {
-    if (document.getElementsByName(name)[0]) {
-        document.getElementsByName(name)[0].placeholder = chrome.i18n.getMessage(key, data);
-    }
+	if (document.getElementsByName(name)[0]) {
+		document.getElementsByName(name)[0].placeholder = chrome.i18n.getMessage(key, data);
+	}
 }
 
 //Translations
