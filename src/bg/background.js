82ae0ffaf7d9c5019a3afc02f81eba33f06801bf
Christopher EnyTC <chris@enytc.com>
2017-04-28 02:07:36 -0300

    Fix background script

--- src/bg/background.js
+++ src/bg/background.js
@@ -7,31 +7,31 @@
 
 
 chrome.runtime.onInstalled.addListener(function(details) {
-    //Redirect
-    chrome.tabs.create({
-        url: chrome.extension.getURL('options/installed.html')
-    });
+	//Redirect
+	chrome.tabs.create({
+		url: chrome.extension.getURL('options/installed.html')
+	});
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    var regex = /^https:\/\/instagram.com\/p\//;
-    //Check if this url is a valid instagram url
-    if (regex.test(tab.url)) {
-        //Get photo link
-        chrome.tabs.sendMessage(tab.id, 'getSource', function(photolink) {
-            if (photolink && photolink !== '') {
-                //Open photo link
-                chrome.tabs.update(tab.id, {
-                    url: photolink
-                });
-            } else {
-                chrome.tabs.duplicate(tab.id, function(dTab) {
-                    chrome.browserAction.setBadgeText({
-                        text: 'click',
-                        tabId: dTab.id
-                    });
-                });
-            }
-        });
-    }
+	var regex = /^https:\/\/www.instagram.com\/p\//;
+	//Check if this url is a valid instagram url
+	if (regex.test(tab.url)) {
+		//Get photo link
+		chrome.tabs.sendMessage(tab.id, 'getInstagramSource', function(photolink) {
+			if (photolink && photolink !== '') {
+				//Open photo link
+				chrome.tabs.update(tab.id, {
+					url: photolink
+				});
+			} else {
+				chrome.tabs.duplicate(tab.id, function(dTab) {
+					chrome.browserAction.setBadgeText({
+						text: 'click',
+						tabId: dTab.id
+					});
+				});
+			}
+		});
+	}
 });
